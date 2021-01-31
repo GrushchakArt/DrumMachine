9077da57fa375e1e860f9aa419b349ea392acd8a
Grushchak Artem <artgrulab@gmail.com>
2021-01-31 22:57:25 +0500

    Готов прототип машины по клику.

--- public/DM.js
+++ public/DM.js
@@ -1,32 +1,15 @@
 
 const buttons = document.getElementsByClassName('button');
-console.log(buttons)
-let audio = document.getElementById("soundBoom")
+const sounds = document.getElementsByClassName('sound');
+let hittedSound;
+let hittedButton;
 
-
-window.addEventListener(document.getElementById("clickBoom").onclick, event => {
-    let audio = document.getElementById("soundBoom")
-    audio.play();
-    alert('sound!')
-});
-
-
-
-//1) подготовка
-//2) получение данных от пользователя
-//3) воспроизведение
-//4) завершение
-
-/**
- * у каждой клавиши есть класс button.
- * * они собраны в массив buttons.
- * К каждой клавише привязан звук и именная клавиша.
-
- * когда именная клавиша или клавиша мыши нажата,
- * соответствующая кнопка получает класс hittedButton
- */
-/*
-    К каждой кнопке должен быть привязан звук и клавиша.
-    При нажатии клавиши или коике по кнопке, звук должен воспроизводиться.
-    Воспроизведение звука прерывает воспроизведение любого другого звука.
-*/
+function drumMachine(button) {
+    let buttonText = button.textContent
+    for (const sound of sounds) {
+        let soundText = sound.textContent
+        if (soundText == buttonText) {
+            sound.play()
+        }
+    }
+}
