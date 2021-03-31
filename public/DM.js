a609ddb62834cfac38b380c4ff083bda6455f61f
Grushchak Artem <artgrulab@gmail.com>
2021-03-31 10:18:54 +0500

    Добавил комментарии. Прибрался. Фиксы по комментам.

--- public/DM.js
+++ public/DM.js
@@ -1,38 +1,53 @@
-
+//Собираем кнопки по классу и трансформируем в массив.
 const buttons = document.getElementsByClassName('button');
 buttonsArr = Array.from(buttons)
 console.log(buttons)
+
+//Собираем звуки по классу и трансформируем в массив.
 const sounds = document.getElementsByClassName('sound');
 soundsArr = Array.from(sounds)
 
+//Функция пробегается по массиву кнопок и ищет кнопку чей текст контент идентичен нажатой кнопке.
+function searchButton(event) {
+    for (const button of buttons) {
+        if (!button.classList.contains('hitted')) {
+            if (event.key == button.textContent) {
+                hittedButtonText = button.textContent
+                button.classList.add('hitted')
+            } else {
+                continue
+            }
+            searchSound(hittedButtonText, button)
+        }
+    }
+}
+
+//Функция проигравает звук если переданный ей текст идентичен текст контенту звука.
+function searchSound(text, button) {
+    let playable;
+    for (const sound of sounds) {
+        if (text == sound.textContent) {
+            playable = sound
+            playable.play()
+        }
+    }
+    //Функция ставит звук на паузу если переданная ей кнопка отжата. И мотает звук на начало.
+    document.addEventListener('keyup', function (event) {
+        button.classList.remove('hitted')
+        playable.pause()
+        playable.currentTime = 0
+    })
+}
+
+//Слушаем нажатие клавиши.
+//Кладем в hittedButton элемент с классоом .hitted
+//Проверяем если hittedButton не пустой то очищаем, иначе запускаем функцию searchButton
 document.addEventListener('keydown', function (event) {
     let hittedButton = this.querySelector('.hitted')
-    let playable;
     console.log(hittedButton)
     if (hittedButton !== null) {
-        //убрать этот иф элс, он не нужен.
         hittedButton.classList.remove('hitted')
     } else {
-        for (const button of buttons) {
-            if (!button.classList.contains('hitted')) {
-                if (event.key == button.textContent) {
-                    hittedButtonText = button.textContent
-                    button.classList.add('hitted')
-                } else {
-                    continue
-                }
-                for (const sound of sounds) {
-                    if (hittedButtonText == sound.textContent) {
-                        playable = sound
-                        playable.play()
-                    }
-                }
-            }
-            document.addEventListener('keyup', function (event) {
-                button.classList.remove('hitted')
-                playable.pause()
-                playable.currentTime = 0
-            })
-        }
+        searchButton(event)
     }
 });
